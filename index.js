e7c0d6efb4321fabad0e4f491c8b83fda3ec7958
Dimas Aji Wicaksono <dimasajiwicaksono@gmail.com>
2021-09-18 13:45:09 +0700

    update config

--- index.js
+++ index.js
@@ -6,7 +6,29 @@ const cors = require("cors");
 require("dotenv").config({ path: __dirname + "/.env" });
 // const jwt = require("./jwt");
 
-require("./db");
+const mongoose = require( 'mongoose' );
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://dbJkt:dbJkt@cluster0.vusuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open');
+});
+
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
 
 var allowedOrigins = ['http://localhost:3000',
   'https://jktbersinar.herokuapp.com/'];
